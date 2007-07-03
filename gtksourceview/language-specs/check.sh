f450343a3a4a85334cd9be08dbacbca2ab66ebda
Paolo Borelli <pborelli@katamail.com>
2007-07-03 20:51:40 +0000

    Awesome haskell support by Duncan Coutts. Bug #453278
    2007-07-03  Paolo Borelli  <pborelli@katamail.com>
    
    	* gtksourceview/language-specs/haddock.lang:
    	* gtksourceview/language-specs/haskell.lang:
    	* gtksourceview/language-specs/haskell-literate.lang:
    
    	Awesome haskell support by Duncan Coutts. Bug #453278
    
    
    svn path=/trunk/; revision=1476

--- gtksourceview/language-specs/check.sh
+++ gtksourceview/language-specs/check.sh
@@ -5,11 +5,12 @@
 
 langs="ada.lang awk.lang changelog.lang chdr.lang c.lang cpp.lang csharp.lang
        css.lang def.lang desktop.lang diff.lang dpatch.lang dtd.lang
-       fortran.lang gap.lang gtk-doc.lang gtkrc.lang haskell.lang
-       html.lang idl.lang ini.lang java.lang javascript.lang latex.lang
-       libtool.lang lua.lang m4.lang makefile.lang objc.lang ocaml.lang
-       ocl.lang octave.lang pascal.lang perl.lang php.lang pkgconfig.lang
-       po.lang python.lang ruby.lang scheme.lang sh.lang sql.lang tcl.lang
+       fortran.lang gap.lang gtk-doc.lang gtkrc.lang haddock.lang
+       haskell.lang haskell-literate.lang html.lang idl.lang ini.lang
+       java.lang javascript.lang latex.lang libtool.lang lua.lang
+       m4.lang makefile.lang objc.lang ocaml.lang ocl.lang octave.lang
+       pascal.lang perl.lang php.lang pkgconfig.lang po.lang
+       python.lang ruby.lang scheme.lang sh.lang sql.lang tcl.lang
        texinfo.lang verilog.lang xml.lang yacc.lang"
 
 styles="gvim.xml kate.xml tango.xml"
