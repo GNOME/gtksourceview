e6104f42d638654fb9e026514e9820b27141c27c
Yevgen Muntyan <muntyan@tamu.edu>
2008-08-02 22:59:25 +0000

    branched for 2.22, un-un-done the last two string changes.
    2008-08-02  Yevgen Muntyan  <muntyan@tamu.edu>
    
    	* gtksourceview/gtksourcecontextengine.c (analyze_line):
    	* gtksourceview/language-specs/forth.lang:
    	branched for 2.22, un-un-done the last two string changes.
    
    
    svn path=/trunk/; revision=1985

--- gtksourceview/gtksourcecontextengine.c
+++ gtksourceview/gtksourcecontextengine.c
@@ -4530,9 +4530,8 @@ analyze_line (GtkSourceContextEngine *ce,
 
 		if (g_timer_elapsed (timer, NULL) * 1000 > MAX_TIME_FOR_ONE_LINE)
 		{
-			/* FIXME mark translatable after string freeze */
-			g_critical ("Highlighting a single line took too much time, "
-				    "syntax highlighting will be disabled");
+			g_critical (_("Highlighting a single line took too much time, "
+				      "syntax highlighting will be disabled"));
 			disable_highlighting (ce);
 			break;
 		}
