1b3568d868222703ca5a7dd0f24f4fd491d71042
Ignacio Casal Quinteiro <nacho.resa@gmail.com>
2009-01-09 13:11:59 +0000

    Add ASP lang file by Fabio Nagao.
    2009-01-09  Ignacio Casal Quinteiro  <nacho.resa@gmail.com>
    
            * gtksourceview/language-specs/asp.lang:
            * gtksourceview/language-specs/Makefile.am:
            * gtksourceview/language-specs/testfiles.sh:
            * po/POTFILES.in:
            Add ASP lang file by Fabio Nagao.
    
    
    svn path=/trunk/; revision=2126

--- gtksourceview/language-specs/testfiles.sh
+++ gtksourceview/language-specs/testfiles.sh
@@ -15,6 +15,27 @@
 dir="testdir"
 mkdir -p $dir/
 
+cat > $dir/file.asp <<EOFEOF
+<html>
+<body>
+
+<%
+dim d
+set d=Server.CreateObject("Scripting.Dictionary")
+d.Add "o", "Orange"
+d.Add "a", "Apple"
+if d.Exists("o")= true then
+    Response.Write("Key exists.")
+else
+    Response.Write("Key does not exist.")
+end if
+set d=nothing
+%>
+
+</body>
+</html>
+EOFEOF
+
 cat > $dir/file.octave <<EOFEOF
 % gtk-source-lang: octave
 % -*- octave -*-
