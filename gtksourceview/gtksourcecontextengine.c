0e4d45143551ac1ac39c65e9b2a71019ba5ea259
Paolo Borelli <pborelli@src.gnome.org>
2009-01-10 18:11:05 +0000

    backport context engine bugfix
    svn path=/branches/gnome-2-24/; revision=2134

--- gtksourceview/gtksourcecontextengine.c
+++ gtksourceview/gtksourcecontextengine.c
@@ -1670,10 +1670,24 @@ gtk_source_context_engine_text_inserted (GtkSourceEngine *engine,
 					 gint             start_offset,
 					 gint             end_offset)
 {
+	GtkTextIter iter;
+	GtkSourceContextEngine *ce = GTK_SOURCE_CONTEXT_ENGINE (engine);
+
 	g_return_if_fail (start_offset < end_offset);
-	invalidate_region (GTK_SOURCE_CONTEXT_ENGINE (engine),
-			   start_offset,
-			   end_offset - start_offset);
+
+	invalidate_region (ce, start_offset, end_offset - start_offset);
+
+	/* If end_offset is at the start of a line (enter key pressed) then
+	 * we need to invalidate the whole new line, otherwise it may not be
+	 * highlighted because the engine analyzes the previous line, end
+	 * context there is none, start context at this line is none too,
+	 * and the engine stops. */
+	gtk_text_buffer_get_iter_at_offset (ce->priv->buffer, &iter, end_offset);
+	if (gtk_text_iter_starts_line (&iter) && !gtk_text_iter_ends_line (&iter))
+	{
+		gtk_text_iter_forward_to_line_end (&iter);
+		invalidate_region (ce, gtk_text_iter_get_offset (&iter), 0);
+	}
 }
 
 /**
