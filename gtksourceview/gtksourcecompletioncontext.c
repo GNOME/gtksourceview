c8b8c3602e006154296a22fce5f40540e5345846
Jesse van den Kieboom <jessevdk@gnome.org>
2009-09-20 23:34:50 +0200

    Removed obsolete gtk_source_completion_context_move_window
    This is now handled properly by 
    gtk_source_completion_provider_get_start_iter instead.

--- gtksourceview/gtksourcecompletioncontext.c
+++ gtksourceview/gtksourcecompletioncontext.c
@@ -330,25 +330,6 @@ gtk_source_completion_context_get_default (GtkSourceCompletionContext *context)
 	
 }
 
-/**
- * gtk_source_completion_context_move_window:
- * @context: A #GtkSourceCompletionContext
- * @iter: A #GtkTextIter
- * 
- * Move the completion window to the specified @iter
- *
- **/
-void 
-gtk_source_completion_context_move_window (GtkSourceCompletionContext *context,
-                                           GtkTextIter                *iter)
-{
-	g_return_if_fail (GTK_IS_SOURCE_COMPLETION_CONTEXT (context));
-	g_return_if_fail (iter != NULL);
-	
-	gtk_source_completion_move_window (context->priv->completion,
-	                                   iter);
-}
-
 void
 _gtk_source_completion_context_cancel (GtkSourceCompletionContext *context)
 {
