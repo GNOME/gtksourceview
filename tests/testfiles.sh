a8fbb8ab2d3267d67f67b696087176ce22e0b10d
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2010-09-23 17:27:18 +0200

    Add Scilab lang file.

--- tests/testfiles.sh
+++ tests/testfiles.sh
@@ -15,6 +15,23 @@
 dir="testdir"
 mkdir -p $dir/
 
+cat > $dir/file.sci << EOFEOF
+// A comment with whites and tabulations 		
+// Scilab editor: http://www.scilab.org/
+
+function [a, b] = myfunction(d, e, f)
+	a = 2.71828 + %pi + f($, :);
+	b = cos(a) + cosh(a);
+	if d == e then
+		b = 10 - e.field;
+	else
+		b = "		test     " + home
+		return
+	end
+	myvar = 1.23e-45;
+endfunction
+EOFEOF
+
 cat > $dir/file.cob << EOFEOF
        IDENTIFICATION DIVISION.
        PROGRAM-ID. Age.
