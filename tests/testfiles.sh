b10d067620880e010d7111f35c6568422642c2ad
Stefan Sundin <recover89@gmail.com>
2012-03-21 20:47:31 +0100

    Adding syntax highlighting for LLVM IR
    https://bugzilla.gnome.org/show_bug.cgi?id=672309

--- tests/testfiles.sh
+++ tests/testfiles.sh
@@ -877,6 +877,27 @@ libdir='/usr/local/gtk/lib/python2.4/site-packages'
 relink_command="(cd /home/muntyan/projects/gtk/build/moo/moo; /bin/sh ../libtool  --tag=CC --mode=relink gcc -g -L/usr/local/gtk/lib -o moo.la -rpath /usr/local/gtk/lib/python2.4/site-packages -no-undefined -module -avoid-version -export-symbols-regex initmoo moopython/libmoomod.la libmoo.la -lutil -L/usr/local/gtk/lib -lgtk-x11-2.0 -lgdk-x11-2.0 -latk-1.0 -lgdk_pixbuf-2.0 -lm -lpangocairo-1.0 -lpango-1.0 -lcairo -lgobject-2.0 -lgmodule-2.0 -ldl -lglib-2.0 -pthread -L/usr/local/gtk/lib -lgthread-2.0 -lglib-2.0 -lpcre -lfam -lxml2 -L/usr/lib/python2.4 -lpython2.4 @inst_prefix_dir@)"
 EOFEOF
 
+cat > $dir/file.ll <<EOFEOF
+; LLVM intermediate representation
+; Run with: llc file.ll && gcc file.s && ./a.out
+
+@str = internal constant [4 x i8] c"%d\0A\00"
+
+declare i32 @printf(i8*, ...)
+
+define i32 @main() {
+  ; Print a secret number on the screen
+  %1 = select i1 true, float 0x402ABD70A0000000, float 0xC0FFEE0000000000
+  %2 = fpext float %1 to double
+  %3 = fmul double %2, 1.000000e+02
+  %4 = fptoui double %3 to i32
+  %5 = add i32 %4, 1
+  ; Call printf
+  call i32 (i8*, ...)* @printf(i8* getelementptr inbounds ([4 x i8]* @str, i32 0, i32 0), i32 %5)
+  ret i32 0
+}
+EOFEOF
+
 cat > $dir/file.pc <<EOFEOF
 # A comment
 prefix=/usr
