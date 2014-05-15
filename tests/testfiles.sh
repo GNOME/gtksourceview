85787b25dfc418ef3d3572f17b74228793e99225
Alexander Vogt <a.vogt@fulguritus.com>
2014-05-15 16:08:08 +0200

    Fortran syntax highlighting improvement
    * Added OpenMP directives to the Fortran language specs
    * Updated testfiles.sh with a Fortran 90/95 program
    
    https://bugzilla.gnome.org/show_bug.cgi?id=730130

--- tests/testfiles.sh
+++ tests/testfiles.sh
@@ -328,12 +328,30 @@ EOFEOF
 
 cat > $dir/file.f <<EOFEOF
 ! gtk-source-lang: fortran
-c comment
-if .TRUE.
-.FALSE.
-endif
-Numbers: 1234 o'10176' b'10101' z'23FF'
-12. .12 12.12 1.23e+09 1.23D+09
+program test95
+  ! Comment: This is a Fortran free-form program
+!$ use omp_lib
+  ! The previous line is not a comment, but an OpenMP directive
+  implicit none
+  character(len=9),parameter  :: t='Some text'
+  integer,parameter           :: a=1
+  real,parameter              :: r1=1.23e0, r2=.12, r3=12., r4=12.12, r5=1.d0
+!$  real*8                    :: t1, t2
+  integer                     :: i
+
+!$ t1 = omp_get_wtime()
+  if ( .true. ) then
+    print *, a, t
+    print *,size( (/ r1, r2, r3, r4, r5 /) )
+  endif
+  !$omp parallel do
+  do i=1,100
+    print *,i
+  enddo ! i
+  !$omp end parallel do
+!$ t2 = omp_get_wtime()
+!$ print *,'The loop took ',t2-t1,'seconds'
+end program
 EOFEOF
 
 cat > $dir/file.frt <<EOFEOF
