835df2b3774c478847a7e06032ebc5caf4dfc7cb
Ilya Novoselov <ilya.novoselov@gmail.com>
2015-05-11 16:50:01 +0500

    Improve sh syntax variable highlighting
    Replace naive match of anything behind $ with contexts, based on posix
    shell syntax specifications.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=749221

--- tests/syntax-highlighting/file.sh
+++ tests/syntax-highlighting/file.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
 echo "Hi there!"
+
+# Parameter Expansion
+
+xxx${xxx}xxx # parameter in braces
+xxx${"{"}xxx xxx${"}"}xxx # brace in quoted string
+
+xxx$0000 # one digit parameter
+
+xxx${parameter:-word}xxx${parameter-word}xxx # Use Default Values
+xxx${parameter:=word}xxx${parameter=word}xxx # Assign Default Values
+xxx${parameter:?word}xxx${parameter?word}xxx # Indicate Error if Null or Unset
+xxx${parameter:?}xxx${parameter?}xxx # Indicate Error if Null or Unset
+xxx${parameter:+word}xxx${parameter+word}xxx # Use Alternative Value
+
+xxx${#parameter}xxx # String Length
+xxx${parameter%word}xxx # Remove Smallest Suffix Pattern
+xxx${parameter%%word}xxx # Remove Largest Suffix Pattern
+xxx${parameter#word}xxx # Remove Smallest Prefix Pattern
+xxx${parameter##word}xxx # Remove Largest Prefix Pattern
+
+xxx${x:-$(ls)}xxx
+xxx${posix:?}xxx
+xxx${3:+posix}xxx
+xxx${#HOME}xxx
+xxx${x%.c}.oxxx
+xxx${x#$HOME}xxx
+xxx${x##*/}xxx
+xxx${x#*}xxx
+xxx${x#"*"}xxx
