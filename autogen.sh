6cfa671c58da6146149226804a9cd6f4da0274fd
Sébastien Wilmet <swilmet@gnome.org>
2015-08-30 12:36:28 +0200

    build: call only intltoolize, not glib-gettextize
    There was an error in the wiki page:
    https://wiki.gnome.org/action/diff/Projects/GnomeCommon/Migration?action=diff&rev1=35&rev2=36
    
    https://wiki.gnome.org/Projects/GnomeCommon/Migration

--- autogen.sh
+++ autogen.sh
@@ -23,7 +23,6 @@ fi
 
 set -x
 aclocal --install || exit 1
-glib-gettextize --force --copy || exit 1
 gtkdocize --copy || exit 1
 intltoolize --force --copy --automake || exit 1
 autoreconf --verbose --force --install -Wno-portability || exit 1
