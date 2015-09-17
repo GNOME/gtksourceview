0583d129c9198b5dce0733d5c9348c56d84f6415
Sébastien Wilmet <swilmet@gnome.org>
2015-09-17 12:34:13 +0200

    Fix license headers in StyleSchemeChooser files

--- gtksourceview/gtksourcestyleschemechooserwidget.h
+++ gtksourceview/gtksourcestyleschemechooserwidget.h
@@ -1,22 +1,22 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8; coding: utf-8 -*-
- * gtksourcestyleschemechooserwidget.h
- * This file is part of gtksourceview
+ *
+ * This file is part of GtkSourceView
  *
  * Copyright (C) 2014 - Christian Hergert
- *               2014 - Ignacio Casal Quinteiro
+ * Copyright (C) 2014 - Ignacio Casal Quinteiro
  *
- * gtksourceview is free software; you can redistribute it and/or
+ * GtkSourceView is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * gtksourceview is distributed in the hope that it will be useful,
+ * GtkSourceView is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with gtksourceview. If not, see <http://www.gnu.org/licenses/>.
+ * along with GtkSourceView. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef GTK_SOURCE_STYLE_SCHEME_CHOOSER_WIDGET_H
