419fb1a6523257a9707bcfcf61b3476914318d4b
Tobias Schönberg <tobias47n9e@gmail.com>
2016-01-31 14:42:04 +0100

    Add highlighting for ES6 Template Strings
    Template strings are part of ES6 and are currently not
    highlighted as strings. This patch adds a context that
    gives template strings the same color as single-quoted
    and double-quoted strings. The brackets of the template
    expressions are given the color of functions. The
    expressions themselves are highlighted as functions
    too.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=758024
    Fixes #758024

--- tests/syntax-highlighting/file.js
+++ tests/syntax-highlighting/file.js
@@ -13,3 +13,15 @@ var matches = /abc/.exec('Alphabet ... that should contain abc, right?');
 a = [thing / thing, thing / thing];
 x = a /b/ c / d;
 
+// Template strings
+// ----------------
+// Template strings are delimited by back-ticks (grave accent) and
+// can span multiple lines. They allow for expressions that inside
+// of a dollar-sign plus curly-bracket construct (${...}).
+
+console.log(`The sum of 2 and 2 is ${2 + 2}`);
+
+let y = 8;
+let my_string = `This is a multiline
+string that also contains
+a template ${y + (4.1 - 2.2)}`;
