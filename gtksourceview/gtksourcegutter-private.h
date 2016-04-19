3ee511e09dd527d0bc247b3982ca20e15b5b7b8f
Sébastien Wilmet <swilmet@gnome.org>
2016-04-19 17:31:07 +0200

    Coding style fixes

--- gtksourceview/gtksourcegutter-private.h
+++ gtksourceview/gtksourcegutter-private.h
@@ -28,13 +28,13 @@
 G_BEGIN_DECLS
 
 G_GNUC_INTERNAL
-GtkSourceGutter *gtk_source_gutter_new (GtkSourceView     *view,
-                                        GtkTextWindowType  type);
+GtkSourceGutter *	gtk_source_gutter_new		(GtkSourceView     *view,
+							 GtkTextWindowType  type);
 
 G_GNUC_INTERNAL
-void gtk_source_gutter_draw (GtkSourceGutter *gutter,
-                             GtkSourceView   *view,
-                             cairo_t         *cr);
+void			gtk_source_gutter_draw		(GtkSourceGutter *gutter,
+							 GtkSourceView   *view,
+							 cairo_t         *cr);
 
 
 G_END_DECLS
