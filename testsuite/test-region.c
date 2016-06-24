ac79360b6407ef1e7898ff4853346b13c50b0a87
Sébastien Wilmet <swilmet@gnome.org>
2016-06-24 22:51:06 +0200

    test-region: #include main header of GtkSourceView
    GtkSourceRegion is now a public class.

--- testsuite/test-region.c
+++ testsuite/test-region.c
@@ -21,8 +21,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include <gtk/gtk.h>
-#include "gtksourceview/gtksourceregion.h"
+#include <gtksourceview/gtksource.h>
 
 static void
 test_region (void)
