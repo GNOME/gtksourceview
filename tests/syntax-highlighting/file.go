424e758a8e68e056eeff7a4eeb74c1da6027b967
Pile Trade <pile.trade@gmail.com>
2019-09-17 15:36:31 +0000

    Update file.go: added tests of new syntaxes and made program valid/runnable https://play.golang.org/p/h6eK-dEBV-4

--- tests/syntax-highlighting/file.go
+++ tests/syntax-highlighting/file.go
@@ -1,8 +1,9 @@
 // gtk-source-lang: Go
 /* comment! */
-var s string := "A string\n"
+package main
 import ("fmt")
-func main() { fmt.Printf(s); }
+var s string = "A string with \x60 a horse \140 \u2014 \U0001F40E %[1]O\n"
+var i = 0b0100_0101i
+func main() { fmt.Printf(s, 0o_7_7_7); }
 type my_struct struct { I int; o string }
-package foo
-bar( a int32, b string )(c float32 ){ c = 1.3 + float32(a - int32(len(b)) }
+func bar( a int32, b string )(c float32 ){ c = 1.3 + 0x1.fp-2 * float32(a - int32(len(b))); return }
