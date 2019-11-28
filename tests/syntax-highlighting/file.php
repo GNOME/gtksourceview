0cb8ec3b94056b566495b9cddf073f5e188d27c9
Jeffery To <jeffery.to@gmail.com>
2019-11-29 06:57:01 +0800

    css.lang (and family): Add embedded-lang-hook contexts
    This allows CSS highlighting inside <style> elements of HTML files to
    end wherever a </style> tag is present, and allows PHP code (<?php ...
    ?>) inside of PHP files to be properly highlighted inside <style>
    elements.

--- tests/syntax-highlighting/file.php
+++ tests/syntax-highlighting/file.php
@@ -24,3 +24,36 @@ a
 b
 ENDING
 END, 'd e f'];
+?>
+
+<!-- PHP embedded: -->
+
+<!-- Inside JavaScript -->
+<script>
+var one = '<?php echo 'Hello World'; ?>';
+var two = '<?php echo "Hello World"; ?>';
+var thr = "<?php echo 'Hello World'; ?>";
+var fou = "<?php echo "Hello World"; ?>";
+
+var num = <?php echo '3'; ?>;
+var re  = /<?php echo 'Hello World'; ?>/;
+var tpl = `<?php echo 'Hello World'; ?>`;
+</script>
+
+<!-- Inside CSS -->
+<style>
+@import url(<?php echo $css_url; ?>);
+@import url('<?php echo $another_url; ?>');
+@import url("<?php echo $some_other_url; ?>");
+
+h1 {
+    border-<?php echo $dir; ?>-color: black;
+    color: <?php echo $color; ?>;
+}
+</style>
+
+<!-- Inside HTML -->
+<img src='<?php echo $Image['one']; ?>' />
+<img src='<?php echo $Image["two"]; ?>' />
+<img src="<?php echo $Image['thr']; ?>" />
+<img src="<?php echo $Image["fou"]; ?>" />
