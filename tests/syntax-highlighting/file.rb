11c4e9df76e603867cb3d236c2db6498b815fe3f
Vlasta Vesely <vlastavesely@protonmail.ch>
2020-01-04 14:02:18 +0100

    ruby.lang: fix highlighting numbers in ranges

--- tests/syntax-highlighting/file.rb
+++ tests/syntax-highlighting/file.rb
@@ -1,3 +1,11 @@
 #!/usr/bin/env ruby
 #
 puts "\141" # Shows 'a'
+
+str = 'lorem'
+puts str[1..3] # 'ore'
+
+# Ranges
+11..11, 0x11..0x11, 01..07, ?a..?f, 1.1..2.2 # range incl. the last value
+11...11, 0x11...0x11, 01...07, ?a...?f, 1.1...2.2 # range excl. the last value
+11....111, 0x11......0x11, 1.1.....1.2 # 4 (and more) dots are not a valid range
