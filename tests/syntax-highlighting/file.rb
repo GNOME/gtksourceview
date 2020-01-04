4230b90f9f89b1c4d5537b6c2d00590aae3d2eb8
Vlasta Vesely <vlastavesely@protonmail.ch>
2020-01-04 14:15:32 +0100

    ruby.lang: fix highlighting of number methods

--- tests/syntax-highlighting/file.rb
+++ tests/syntax-highlighting/file.rb
@@ -5,6 +5,10 @@ puts "\141" # Shows 'a'
 str = 'lorem'
 puts str[1..3] # 'ore'
 
+# Number methods
+puts -11.to_s + ' ' + 0x11.to_s  + ' ' + 1.1.to_s + ' ' + ?a.to_s + ' ' + 1.x
+11.1aa # not a valid method name
+
 # Ranges
 11..11, 0x11..0x11, 01..07, ?a..?f, 1.1..2.2 # range incl. the last value
 11...11, 0x11...0x11, 01...07, ?a...?f, 1.1...2.2 # range excl. the last value
