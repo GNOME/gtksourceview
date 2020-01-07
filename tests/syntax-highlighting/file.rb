ec30a7e15e2d9b6dbf12c0619e75a15261d1561b
Jeffery To <jeffery.to@gmail.com>
2020-01-08 04:10:48 +0800

    ruby.lang: Update escape sequence highlighting
    This updates highlighting to match the allowed escape sequences in Ruby
    2.7.0.
    
    References:
    * https://ruby-doc.org/core-2.7.0/doc/syntax/literals_rdoc.html#label-Strings
    * https://idiosyncratic-ruby.com/61-meta-escape-control.html

--- tests/syntax-highlighting/file.rb
+++ tests/syntax-highlighting/file.rb
@@ -5,6 +5,26 @@ puts "\141" # Shows 'a'
 str = 'lorem'
 puts str[1..3] # 'ore'
 
+# Escape sequences
+"\a \b \t \n \v \f \r \e \s" # control characters
+"\\" # backslash
+"\1 \11 \111" # octal
+"\xa \xA1" # hexadecimal
+"\u9aBC \u{1} \u{10FFFF} \u{123 abc DEF}" # unicode
+"\ca     \c"     \c\1     \c\11     \c\111     \c\xa     \c\xA1"     # control characters
+"\C-a    \C-"    \C-\1    \C-\11    \C-\111    \C-\xa    \C-\xA1"    # control characters
+"\M-a    \M-"    \M-\1    \M-\11    \M-\111    \M-\xa    \M-\xA1"    # meta characters
+"\M-\ca  \M-\c"  \M-\c\1  \M-\c\11  \M-\c\111  \M-\c\xa  \M-\c\xA1"  # meta control characters
+"\c\M-a  \c\M-"  \c\M-\1  \c\M-\11  \c\M-\111  \c\M-\xa  \c\M-\xA1"  # meta control characters
+"\M-\C-a \M-\C-" \M-\C-\1 \M-\C-\11 \M-\C-\111 \M-\C-\xa \M-\C-\xA1" # meta control characters
+"\C-\M-a \C-\M-" \C-\M-\1 \C-\M-\11 \C-\M-\111 \C-\M-\xa \C-\M-\xA1" # meta control characters
+
+# Invalid escape sequences
+"\xg"
+"\ug \u{1234567}"
+"\C \c\C-a \C-\ca \c\M-\ca \c\M-\C-a \C-\M-\ca"
+"\M \M-\M-a \M-\c\M-a \M-\C-\M-a"
+
 # Number methods
 puts -11.to_s + ' ' + 0x11.to_s  + ' ' + 1.1.to_s + ' ' + ?a.to_s + ' ' + 1.x
 11.1aa # not a valid method name
