6b04c4d472f8389c1f02c913494ff88de4356434
Christian Hergert <chergert@redhat.com>
2020-01-08 17:02:05 -0800

    completion: modernize GtkSourceCompletionInfo
    Make final and switch to G_DECLARE_FINAL_TYPE()

--- gtksourceview/gtksourcecompletioninfo.h
+++ gtksourceview/gtksourcecompletioninfo.h
@@ -30,45 +30,19 @@
 
 G_BEGIN_DECLS
 
-#define GTK_SOURCE_TYPE_COMPLETION_INFO             (gtk_source_completion_info_get_type ())
-#define GTK_SOURCE_COMPLETION_INFO(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), GTK_SOURCE_TYPE_COMPLETION_INFO, GtkSourceCompletionInfo))
-#define GTK_SOURCE_COMPLETION_INFO_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), GTK_SOURCE_TYPE_COMPLETION_INFO, GtkSourceCompletionInfoClass)
-#define GTK_SOURCE_IS_COMPLETION_INFO(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GTK_SOURCE_TYPE_COMPLETION_INFO))
-#define GTK_SOURCE_IS_COMPLETION_INFO_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), GTK_SOURCE_TYPE_COMPLETION_INFO))
-#define GTK_SOURCE_COMPLETION_INFO_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), GTK_SOURCE_TYPE_COMPLETION_INFO, GtkSourceCompletionInfoClass))
-
-typedef struct _GtkSourceCompletionInfoPrivate GtkSourceCompletionInfoPrivate;
-
-struct _GtkSourceCompletionInfo
-{
-	GtkWindow parent;
-
-	GtkSourceCompletionInfoPrivate *priv;
-};
-
-typedef struct _GtkSourceCompletionInfoClass GtkSourceCompletionInfoClass;
-
-struct _GtkSourceCompletionInfoClass
-{
-	GtkWindowClass parent_class;
-
-	gpointer padding[10];
-};
+#define GTK_SOURCE_TYPE_COMPLETION_INFO (gtk_source_completion_info_get_type())
 
 GTK_SOURCE_AVAILABLE_IN_ALL
-GType		 gtk_source_completion_info_get_type		(void) G_GNUC_CONST;
+G_DECLARE_FINAL_TYPE (GtkSourceCompletionInfo, gtk_source_completion_info, GTK_SOURCE, COMPLETION_INFO, GtkWindow)
 
 GTK_SOURCE_AVAILABLE_IN_ALL
-GtkSourceCompletionInfo *
-		 gtk_source_completion_info_new			(void);
-
+GtkSourceCompletionInfo *gtk_source_completion_info_new          (void);
 GTK_SOURCE_AVAILABLE_IN_ALL
-void		 gtk_source_completion_info_move_to_iter	(GtkSourceCompletionInfo *info,
-								 GtkTextView             *view,
-								 GtkTextIter             *iter);
-
+void                     gtk_source_completion_info_move_to_iter (GtkSourceCompletionInfo *info,
+                                                                  GtkTextView             *view,
+                                                                  GtkTextIter             *iter);
 G_GNUC_INTERNAL
-void		 _gtk_source_completion_info_set_xoffset	(GtkSourceCompletionInfo *info,
-								 gint                     xoffset);
+void                     _gtk_source_completion_info_set_xoffset (GtkSourceCompletionInfo *info,
+                                                                  gint                     xoffset);
 
 G_END_DECLS
