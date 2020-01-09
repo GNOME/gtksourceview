3fc476ac498d4ede093fd8cb6a1ee23a3949de98
Christian Hergert <chergert@redhat.com>
2020-01-09 09:52:38 -0800

    map: modernize GtkSourceMap
     - Use G_DECLARE_ macros

--- gtksourceview/gtksourcemap.h
+++ gtksourceview/gtksourcemap.h
@@ -29,39 +29,25 @@
 
 G_BEGIN_DECLS
 
-#define GTK_SOURCE_TYPE_MAP            (gtk_source_map_get_type())
-#define GTK_SOURCE_MAP(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GTK_SOURCE_TYPE_MAP, GtkSourceMap))
-#define GTK_SOURCE_MAP_CONST(obj)      (G_TYPE_CHECK_INSTANCE_CAST ((obj), GTK_SOURCE_TYPE_MAP, GtkSourceMap const))
-#define GTK_SOURCE_MAP_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass),  GTK_SOURCE_TYPE_MAP, GtkSourceMapClass))
-#define GTK_SOURCE_IS_MAP(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GTK_SOURCE_TYPE_MAP))
-#define GTK_SOURCE_IS_MAP_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  GTK_SOURCE_TYPE_MAP))
-#define GTK_SOURCE_MAP_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),  GTK_SOURCE_TYPE_MAP, GtkSourceMapClass))
+#define GTK_SOURCE_TYPE_MAP (gtk_source_map_get_type())
 
-typedef struct _GtkSourceMapClass GtkSourceMapClass;
-
-struct _GtkSourceMap
-{
-	GtkSourceView parent_instance;
-};
+GTK_SOURCE_AVAILABLE_IN_3_18
+G_DECLARE_DERIVABLE_TYPE (GtkSourceMap, gtk_source_map, GTK_SOURCE, MAP, GtkSourceView)
 
 struct _GtkSourceMapClass
 {
 	GtkSourceViewClass parent_class;
 
-	gpointer padding[10];
+	/*< private >*/
+	gpointer _reserved[10];
 };
 
 GTK_SOURCE_AVAILABLE_IN_3_18
-GType			 gtk_source_map_get_type	(void);
-
+GtkWidget     *gtk_source_map_new      (void);
 GTK_SOURCE_AVAILABLE_IN_3_18
-GtkWidget		*gtk_source_map_new		(void);
-
+void           gtk_source_map_set_view (GtkSourceMap  *map,
+                                        GtkSourceView *view);
 GTK_SOURCE_AVAILABLE_IN_3_18
-void			 gtk_source_map_set_view	(GtkSourceMap  *map,
-							 GtkSourceView *view);
-
-GTK_SOURCE_AVAILABLE_IN_3_18
-GtkSourceView		*gtk_source_map_get_view	(GtkSourceMap  *map);
+GtkSourceView *gtk_source_map_get_view (GtkSourceMap  *map);
 
 G_END_DECLS
