9c06ee3464f1b9c6c5cb822780809f82334df45f
Christian Hergert <chergert@redhat.com>
2020-01-09 09:22:40 -0800

    language: modernize GtkSourceLanguageManager
     - Use G_DECLARE_ macros
     - Make object final
     - Fix spacing style for multi-line functions

--- gtksourceview/gtksourcelanguagemanager.h
+++ gtksourceview/gtksourcelanguagemanager.h
@@ -29,61 +29,30 @@
 
 G_BEGIN_DECLS
 
-#define GTK_SOURCE_TYPE_LANGUAGE_MANAGER		(gtk_source_language_manager_get_type ())
-#define GTK_SOURCE_LANGUAGE_MANAGER(obj)		(G_TYPE_CHECK_INSTANCE_CAST((obj), GTK_SOURCE_TYPE_LANGUAGE_MANAGER, GtkSourceLanguageManager))
-#define GTK_SOURCE_LANGUAGE_MANAGER_CLASS(klass)	(G_TYPE_CHECK_CLASS_CAST((klass), GTK_SOURCE_TYPE_LANGUAGE_MANAGER, GtkSourceLanguageManagerClass))
-#define GTK_SOURCE_IS_LANGUAGE_MANAGER(obj)		(G_TYPE_CHECK_INSTANCE_TYPE((obj), GTK_SOURCE_TYPE_LANGUAGE_MANAGER))
-#define GTK_SOURCE_IS_LANGUAGE_MANAGER_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), GTK_SOURCE_TYPE_LANGUAGE_MANAGER))
-#define GTK_SOURCE_LANGUAGE_MANAGER_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS((obj), GTK_SOURCE_TYPE_LANGUAGE_MANAGER, GtkSourceLanguageManagerClass))
-
-
-typedef struct _GtkSourceLanguageManagerClass	GtkSourceLanguageManagerClass;
-typedef struct _GtkSourceLanguageManagerPrivate GtkSourceLanguageManagerPrivate;
-
-struct _GtkSourceLanguageManager
-{
-	GObject parent_instance;
-
-	GtkSourceLanguageManagerPrivate *priv;
-};
-
-struct _GtkSourceLanguageManagerClass
-{
-	GObjectClass parent_class;
-
-	/* Padding for future expansion */
-	gpointer padding[10];
-};
+#define GTK_SOURCE_TYPE_LANGUAGE_MANAGER (gtk_source_language_manager_get_type())
 
 GTK_SOURCE_AVAILABLE_IN_ALL
-GType			  gtk_source_language_manager_get_type			(void) G_GNUC_CONST;
+G_DECLARE_FINAL_TYPE (GtkSourceLanguageManager, gtk_source_language_manager, GTK_SOURCE, LANGUAGE_MANAGER, GObject)
 
 GTK_SOURCE_AVAILABLE_IN_ALL
-GtkSourceLanguageManager *gtk_source_language_manager_new			(void);
-
+GtkSourceLanguageManager *gtk_source_language_manager_new              (void);
 GTK_SOURCE_AVAILABLE_IN_ALL
-GtkSourceLanguageManager *gtk_source_language_manager_get_default		(void);
-
+GtkSourceLanguageManager *gtk_source_language_manager_get_default      (void);
 GTK_SOURCE_AVAILABLE_IN_ALL
-const gchar * const *	  gtk_source_language_manager_get_search_path		(GtkSourceLanguageManager *lm);
-
+const gchar * const      *gtk_source_language_manager_get_search_path  (GtkSourceLanguageManager  *lm);
 GTK_SOURCE_AVAILABLE_IN_ALL
-void			  gtk_source_language_manager_set_search_path		(GtkSourceLanguageManager *lm,
-										 gchar                   **dirs);
-
+void                      gtk_source_language_manager_set_search_path  (GtkSourceLanguageManager  *lm,
+                                                                        gchar                    **dirs);
 GTK_SOURCE_AVAILABLE_IN_ALL
-const gchar * const *	  gtk_source_language_manager_get_language_ids		(GtkSourceLanguageManager *lm);
-
+const gchar * const      *gtk_source_language_manager_get_language_ids (GtkSourceLanguageManager  *lm);
 GTK_SOURCE_AVAILABLE_IN_ALL
-GtkSourceLanguage	 *gtk_source_language_manager_get_language		(GtkSourceLanguageManager *lm,
-										 const gchar              *id);
-
+GtkSourceLanguage        *gtk_source_language_manager_get_language     (GtkSourceLanguageManager  *lm,
+                                                                        const gchar               *id);
 GTK_SOURCE_AVAILABLE_IN_ALL
-GtkSourceLanguage	 *gtk_source_language_manager_guess_language		(GtkSourceLanguageManager *lm,
-										 const gchar		  *filename,
-										 const gchar		  *content_type);
-
+GtkSourceLanguage        *gtk_source_language_manager_guess_language   (GtkSourceLanguageManager  *lm,
+                                                                        const gchar               *filename,
+                                                                        const gchar               *content_type);
 G_GNUC_INTERNAL
-GtkSourceLanguageManager *_gtk_source_language_manager_peek_default		(void);
+GtkSourceLanguageManager *_gtk_source_language_manager_peek_default    (void);
 
 G_END_DECLS
