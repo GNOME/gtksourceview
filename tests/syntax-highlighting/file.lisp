444ba4c38ad1e7fbfff594be6f8b9536e0e06f5a
Tarn Burton <twburton@gmail.com>
2020-02-11 15:22:54 -0500

    commonlisp.lang: Add test file

---
+++ tests/syntax-highlighting/file.lisp
@@ -0,0 +1,24 @@
+; Define a simple package
+(defpackage #:fu
+  (:use #:alexandria #:cl)
+  (:export
+     #:bar))
+
+(in-package :foo)
+
+#|
+  Block comment
+  more text
+|#
+
+(defmacro bar (quux &rest wibble)
+  "String with some an escape \""
+  `(angle ,quux ,@wibble))
+
+(bar 123 234. 745e7 126f4 #3r12 #xaa0 #O23 #b0101/11
+     #< ; invalid sharpsign
+     #\; #\Spac\e ; Some characters
+     #10*01010101 ; bit vector
+     'baz #'gronk)
+
+
