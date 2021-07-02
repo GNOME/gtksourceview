0b1f3e69660017ad328967638ec411bbdb3b0f6e
Christian Hergert <chergert@redhat.com>
2021-07-02 12:35:58 -0700

    implregex: cleanup whitespace

--- gtksourceview/implregex-private.h
+++ gtksourceview/implregex-private.h
@@ -33,48 +33,29 @@ typedef gboolean (*ImplRegexEvalCallback) (const ImplMatchInfo *match_info,
                                            gpointer             user_data);
 
 
-ImplRegex  *impl_regex_new                  (const char             *pattern,
-                                             GRegexCompileFlags      compile_options,
-                                             GRegexMatchFlags        match_options,
-                                             GError                **error);
-gboolean    impl_regex_match                (const ImplRegex        *regex,
-                                             const char             *string,
-                                             GRegexMatchFlags        match_options,
-                                             ImplMatchInfo         **match_info);
-ImplRegex  *impl_regex_ref                  (ImplRegex              *regex);
-void        impl_regex_unref                (ImplRegex              *regex);
-void        impl_match_info_free            (ImplMatchInfo          *match_info);
-char       *impl_match_info_fetch           (const ImplMatchInfo    *match_info,
-                                             int                     match_num);
-char       *impl_match_info_fetch_named     (const ImplMatchInfo    *match_info,
-                                             const char             *name);
-char       *impl_regex_replace_eval         (const ImplRegex        *regex,
-                                             const char             *string,
-                                             gssize                  string_len,
-                                             gsize                   start_position,
-                                             GRegexMatchFlags        match_options,
-                                             ImplRegexEvalCallback   eval,
-                                             gpointer                user_data,
-                                             GError                **error);
-gboolean    impl_regex_match_full           (const ImplRegex        *regex,
-                                             const char             *string,
-                                             gssize                  string_len,
-                                             gsize                   start_position,
-                                             GRegexMatchFlags        match_options,
-                                             ImplMatchInfo         **match_info,
-                                             GError                **error);
-gboolean    impl_match_info_fetch_pos       (const ImplMatchInfo    *match_info,
-                                             guint                   match_num,
-                                             int                    *start_pos,
-                                             int                    *end_pos);
-gboolean    impl_match_info_fetch_named_pos (const ImplMatchInfo    *match_info,
-                                             const char             *name,
-                                             int                    *start_pos,
-                                             int                    *end_pos);
-gboolean    impl_match_info_matches         (const ImplMatchInfo    *match_info);
-gboolean    impl_match_info_next            (ImplMatchInfo          *match_info,
-                                             GError                **error);
-const char *impl_regex_get_pattern          (const ImplRegex        *regex);
+ImplRegex  *impl_regex_new                   (const char             *pattern,
+                                              GRegexCompileFlags      compile_options,
+                                              GRegexMatchFlags        match_options,
+                                              GError                **error);
+gboolean    impl_regex_match                 (const ImplRegex        *regex,
+                                              const char             *string,
+                                              GRegexMatchFlags        match_options,
+                                              ImplMatchInfo         **match_info);
+ImplRegex  *impl_regex_ref                   (ImplRegex              *regex);
+void        impl_regex_unref                 (ImplRegex              *regex);
+void        impl_match_info_free             (ImplMatchInfo          *match_info);
+char       *impl_match_info_fetch            (const ImplMatchInfo    *match_info,
+                                              int                     match_num);
+char       *impl_match_info_fetch_named      (const ImplMatchInfo    *match_info,
+                                              const char             *name);
+char       *impl_regex_replace_eval          (const ImplRegex        *regex,
+                                              const char             *string,
+                                              gssize                  string_len,
+                                              gsize                   start_position,
+                                              GRegexMatchFlags        match_options,
+                                              ImplRegexEvalCallback   eval,
+                                              gpointer                user_data,
+                                              GError                **error);
 char       *impl_regex_replace               (const ImplRegex        *regex,
                                               const char             *string,
                                               gssize                  string_len,
@@ -82,8 +63,27 @@ char       *impl_regex_replace               (const ImplRegex        *regex,
                                               const char             *replacement,
                                               GRegexMatchFlags        match_options,
                                               GError                **error);
+gboolean    impl_regex_match_full            (const ImplRegex        *regex,
+                                              const char             *string,
+                                              gssize                  string_len,
+                                              gsize                   start_position,
+                                              GRegexMatchFlags        match_options,
+                                              ImplMatchInfo         **match_info,
+                                              GError                **error);
+gboolean    impl_match_info_fetch_pos        (const ImplMatchInfo    *match_info,
+                                              guint                   match_num,
+                                              int                    *start_pos,
+                                              int                    *end_pos);
+gboolean    impl_match_info_fetch_named_pos  (const ImplMatchInfo    *match_info,
+                                              const char             *name,
+                                              int                    *start_pos,
+                                              int                    *end_pos);
 gboolean    impl_match_info_is_partial_match (const ImplMatchInfo    *match_info);
+gboolean    impl_match_info_matches          (const ImplMatchInfo    *match_info);
+gboolean    impl_match_info_next             (ImplMatchInfo          *match_info,
+                                              GError                **error);
 int         impl_match_info_get_match_count  (const ImplMatchInfo    *match_info);
+const char *impl_regex_get_pattern           (const ImplRegex        *regex);
 int         impl_regex_get_max_lookbehind    (const ImplRegex        *regex);
 
 G_END_DECLS
