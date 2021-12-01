1c8e4d7987c1a31c9abcd4c634562c9e3ac74eb0
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-12-01 12:52:23 +0100

    docs: add a urlmap
    shamelessly copied from gtk docs

---
+++ docs/urlmap.js
@@ -0,0 +1,12 @@
+
+// A map between namespaces and base URLs for their online documentation
+baseURLs = [
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'Gio', 'https://docs.gtk.org/gio/' ],
+    [ 'Gdk', 'https://docs.gtk.org/gdk4/' ],
+    [ 'Gsk', 'https://docs.gtk.org/gsk4/' ],
+    [ 'Gtk', 'https://docs.gtk.org/gtk4/' ],
+    [ 'Pango', 'https://docs.gtk.org/Pango/' ],
+    [ 'GdkPixbuf', 'https://docs.gtk.org/gdk-pixbuf/' ],
+]
