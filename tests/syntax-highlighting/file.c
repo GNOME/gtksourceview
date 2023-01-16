9b47ee5bdaa2bbc0f84442e5467676318c5c1296
Vlasta Vesely <vlastavesely@protonmail.ch>
2023-01-16 19:48:45 +0000

    c.lang: highlight binary numbers

--- tests/syntax-highlighting/file.c
+++ tests/syntax-highlighting/file.c
@@ -8,6 +8,13 @@ int main (void)
     int b = 089;
     int c = 89.;
     int d = 'a';
+
+    int a = 0b0101U;
+    int c = 0b0101L;
+    int d = 0B0101L;
+    int b = 0b201u;
+    int b = 0B061u;
+
     double hexadecimal_floating_constant = 0x1.2p3;
     printf ("Hello %s!\n", "world");
     return 0;
